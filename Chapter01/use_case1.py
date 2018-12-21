5b7cf766e7dd9798a712a179a37a8412b61efd13
swathym14 <32697994+swathym14@users.noreply.github.com>
2018-12-21 15:09:14 +0530

    Update use_case1.py

--- Chapter01/use_case1.py
+++ Chapter01/use_case1.py
@@ -24,7 +24,7 @@ p = getpass.getpass(prompt="Enter your password: ")
 creds=uname+"*.*"+p
 
 #encrypted creds of the registered customers
-#for testing username:password is customer1:password1 , customer2:password2, etc...
+#for testing username:password is customer1:password1 , customer2:password2, and so on...
 
 #create a dictionary:
 customers={
